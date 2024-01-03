bc0fc7c398435b1df7236414a90548c758c29867
Chaima BEKHAOUDA <chaima.bekhaouda@laplateforme.io>
2024-01-03 22:11:52 +0100

    added Job3

---
+++ Jour2/Job3.py
@@ -0,0 +1,48 @@
+class Livre:
+    def __init__(self, titre, auteur, n_pages, disponible = True):
+        self.__titre = titre
+        self.__auteur = auteur
+        self.__n_pages = n_pages
+        self.__disponible = disponible
+
+    def getTitre(self):
+        return self.__titre
+
+    def getAuteur(self):
+       return self.__auteur
+
+    def getN_pages(self):
+        return self.__n_pages
+
+    def getDisponible(self):
+        return self.__disponible
+
+    def setTitre(self, titre):
+        self.__titre = titre
+
+    def setAuteur(self, auteur):
+        self.__auteur = auteur
+
+    def setN_pages(self, n_pages):
+        if n_pages > 0:
+            self.__n_pages = n_pages
+        else:
+            print("Erreur: Le nombre de pages doit être positif")
+
+    def setDisponible(self, disponible):
+        self.__disponible = disponible
+
+    def verification(self):
+        return self.__disponible
+
+    def emprunter(self):
+        if self.verification():
+            self.__disponible = False
+        else:
+            print("Erreur: Le livre n'est pas disponible")
+
+    def rendre(self):
+        if self.verification():
+            print("Erreur: Le livre est déjà disponible")
+        else:
+            self.__disponible = True
