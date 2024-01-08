77180a4aed288e60639b02b3f146629d20e8cf18
Chaima BEKHAOUDA <chaima.bekhaouda@laplateforme.io>
2024-01-08 11:43:13 +0100

    Jour4: fix Job6

--- Jour4/Job6.py
+++ Jour4/Job6.py
@@ -4,7 +4,7 @@ class Vehicule:
         self.modele = modele
         self.annee = annee
         self.prix = prix
-    
+
     def informationsVehicule(self):
         print(f"Marque : {self.marque}")
         print(f"Modèle : {self.modele}")
@@ -16,12 +16,26 @@ class Voiture(Vehicule):
     def __init__(self, marque, modele, annee, prix):
         super().__init__(marque, modele, annee, prix)
         self.portes = 4
-    
+
     def informationsVehicule(self):
         super().informationsVehicule()
         print(f"Nombre de portes : {self.portes}")
 
 
+class Moto(Vehicule):
+    def __init__(self, marque, modele, annee, prix):
+        super().__init__(marque, modele, annee, prix)
+        self.roues = 2
+
+    def informationsVehicule(self):
+        super().informationsVehicule()
+        print(f"Nombre de roues : {self.roues}")
+
+
 # Instanciation de l'objet Voiture
 voiture = Voiture("Mercedes", "Classe A", 2020, 18500)
 voiture.informationsVehicule()
+
+# Instanciation de l'objet Moto
+moto = Moto("Honda", "CBR500R", 2021, 7500)
+moto.informationsVehicule()
